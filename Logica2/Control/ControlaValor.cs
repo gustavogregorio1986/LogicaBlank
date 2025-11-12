f2963e144124f850f8362e5462a2bb000c9bb59b
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-11-12 13:34:10 -0300

    Desenvolvimento dfa logica de programação

---
+++ Logica2/Control/ControlaValor.cs
@@ -0,0 +1,43 @@
+﻿using Logica2.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica2.Control
+{
+    public class ControlaValor
+    {
+        public void ValorMetodo(Valor valor)
+        {
+            switch (valor.Propriedade) { 
+                 case "segunda":
+                    Console.WriteLine("Valor é segunda dia de semana");
+                    break;
+                case "terça":
+                    Console.WriteLine("Valor é terça dia de semana");
+                    break;
+                case "quarta":
+                    Console.WriteLine("Valor é quarta dia de semana");
+                    break;
+                case "quinta":
+                    Console.WriteLine("Valor é quinta dia de semana");
+                    break;
+                case "sexta":
+                    Console.WriteLine("Valor é sexta dia de semana");
+                    break;
+                case "sabado":
+                    Console.WriteLine("Valor é sabado fim de semana");
+                    break;
+                case "domingo":
+                    Console.WriteLine("Valor é domingo fim de semana");
+                    break;
+                default:
+                    Console.WriteLine("Não é valida essa opção");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Logica2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Logica2.Control;
+using Logica2.Entidade;
+
+namespace Logica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Valor valor = new Valor();
+            valor.Propriedade = "segunda";
+            Console.WriteLine("Propriedade: " + valor.Propriedade);
+
+            ControlaValor controlaValor = new ControlaValor();
+            controlaValor.ValorMetodo(valor);
+        }
+    }
+}
