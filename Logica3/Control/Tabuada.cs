624efbc0036f72809fa498928f9db890c7eb4db0
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-11-12 14:22:19 -0300

    Logica de programação desenvolvida

---
+++ Logica3/Control/Tabuada.cs
@@ -0,0 +1,24 @@
+﻿using Logica3.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica3.Control
+{
+    public class Tabuada
+    {
+        public void ExibirTabuada(Valor valor)
+        {
+            Console.WriteLine($"Tabuada do {valor.Numero}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = valor.Numero * i;
+                Console.WriteLine($"{valor.Numero} x {i} = {resultado}");
+            }
+        }
+
+
+    }
+}
---
+++ Logica3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Logica3.Control;
+using Logica3.Entidade;
+using System;
+
+namespace Logica3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Valor valor = new Valor();
+            valor.Numero = 7;
+            Console.WriteLine("Valor é: " + valor.Numero);
+            Tabuada tabuada = new Tabuada();
+            tabuada.ExibirTabuada(valor);
+        }
+    }
+}
