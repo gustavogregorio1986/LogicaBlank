3d55e9e38a7793376fdae991704a2a7a0e73a9b0
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-11-12 11:25:31 -0300

    Add project files.

---
+++ Logica/Control/ControlaValor.cs
@@ -0,0 +1,40 @@
+﻿using Logica.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Control
+{
+    public class ControlaValor
+    {
+        public double Somar(Valor valor)
+        {
+            double soma = 0;
+            soma = valor.Numero1 + valor.Numero2;
+            return soma;
+        }
+
+        public double Subtrair(Valor valor)
+        {
+            double subtracao = 0;
+            subtracao = valor.Numero1 - valor.Numero2;
+            return subtracao;
+        }
+
+        public double Multiplicar(Valor valor)
+        {
+            double multiplicacao = 0;
+            multiplicacao = valor.Numero1 * valor.Numero2;
+            return multiplicacao;
+        }
+
+        public double Dividir(Valor valor)
+        {
+            double divisao = 0;
+            divisao = valor.Numero1 / valor.Numero2;
+            return divisao;
+        }
+    }
+}
---
+++ Logica/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Logica.Control;
+using Logica.Entidade;
+
+namespace Logica
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Valor valor = new Valor();
+             valor.Numero1 = 10;
+             valor.Numero2 = 5;
+
+            Console.WriteLine("Numero1: " + valor.Numero1);
+            Console.WriteLine("Numero2: " + valor.Numero2);
+
+            ControlaValor controla = new ControlaValor();
+            Console.WriteLine("Soma: " + controla.Somar(valor));
+            Console.WriteLine("Subtração: " + controla.Subtrair(valor));
+            Console.WriteLine("Multiplicação: " + controla.Multiplicar(valor));
+            Console.WriteLine("Divisão: " + controla.Dividir(valor));
+        }
+    }
+}
